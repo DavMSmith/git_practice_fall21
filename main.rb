d3e23862e5d8cf75fcc97b5402d883bdd95935f9
DavMSmith <davonda.smith@gmail.com>
2021-08-24 22:03:36 -0400

    Added subtraction

---
+++ main.rb
@@ -0,0 +1,15 @@
+## add minus subtract multiply
+
+def add(num,num2)
+  num1 + num2
+end
+
+puts add(1,1)
+puts add(2,2)git add 
+puts add (4,4)
+
+def minus (num1, num2)
+  num1-num2
+end
+puts minus(1,1)
+end
